47fa47dfcb3ec52b6db05830e5f18e710395a489
ArkJzzz <arkjzzz@gmail.com>
2021-08-04 22:55:46 +0300

    Add model Complaints in admin page

--- property/admin.py
+++ property/admin.py
@@ -1,25 +1,41 @@
 from django.contrib import admin
 
 from property.models import Flat
-
+from property.models import Complaint
 
 class FlatAdmin(admin.ModelAdmin):
-	search_fields = ['town', 'address', 'owner']
-	readonly_fields = ['created_at']
-	list_display = [
-			'town',
-			'address',
-			'price',
-			'new_building',
-			'construction_year',
-		]
-	list_editable = ['new_building']
-	list_filter = [
-			'new_building',
-			'rooms_number',
-			'has_balcony',
-			'floor',
-		]
+    search_fields = ['town', 'address', 'owner']
+    readonly_fields = ['created_at']
+    list_display = [
+            'town',
+            'address',
+            'price',
+            'new_building',
+            'construction_year',
+        ]
+    list_editable = ['new_building']
+    list_filter = [
+            'new_building',
+            'rooms_number',
+            'has_balcony',
+            'floor',
+        ]
+    
+
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = [
+            'user',
+            'flat',
+            'text',
+        ]
+    list_editable = [
+            'text',
+        ]
+    raw_id_fields = [
+            'user',
+            'flat',
+        ]
 
 
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
