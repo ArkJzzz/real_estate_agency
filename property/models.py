1b9b238b2613000f686098d1e2f9ffefb7e0f2df
ArkJzzz <arkjzzz@gmail.com>
2021-08-30 21:04:59 +0300

    Add related_name for all relationships

--- property/models.py
+++ property/models.py
@@ -11,8 +11,14 @@ class Flat(models.Model):
             default=timezone.now,
             db_index=True,
         )
-    description = models.TextField('Текст объявления', blank=True)
-    price = models.IntegerField('Цена квартиры', db_index=True)
+    description = models.TextField(
+            'Текст объявления',
+            blank=True,
+        )
+    price = models.IntegerField(
+            'Цена квартиры',
+            db_index=True,
+        )
     town = models.CharField(
             'Город, где находится квартира',
             max_length=50,
@@ -34,44 +40,60 @@ class Flat(models.Model):
             help_text='Первый этаж, последний этаж, пятый этаж',
         )
     rooms_number = models.IntegerField(
-        'Количество комнат в квартире',
-        db_index=True)
+            'Количество комнат в квартире',
+            db_index=True,
+        )
     living_area = models.IntegerField(
             'количество жилых кв.метров',
             null=True,
             blank=True,
             db_index=True,
         )
-    has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
-    active = models.BooleanField('Активно-ли объявление', db_index=True)
+    has_balcony = models.NullBooleanField(
+            'Наличие балкона',
+            db_index=True,
+        )
+    active = models.BooleanField(
+            'Активно-ли объявление',
+            db_index=True,
+        )
     construction_year = models.IntegerField(
             'Год постройки здания',
             null=True,
             blank=True,
             db_index=True,
         )
-    new_building = models.NullBooleanField('Новостройка', db_index=True)
+    new_building = models.NullBooleanField(
+            'Новостройка',
+            db_index=True,
+        )
     liked_by = models.ManyToManyField(
             User,
             verbose_name='Кто лайкнул',
             blank=True,
+            related_name='liked_flats',
         )
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
 
 class Complaint(models.Model):
     user = models.ForeignKey(
             User,
             on_delete=models.CASCADE,
             verbose_name='Кто жаловался',
+            related_name='user_complaints',
         )
     flat = models.ForeignKey(
             Flat,
             on_delete=models.CASCADE,
             verbose_name='Квартира, на которую пожаловались',
+            related_name='flat_complaints',
         )
-    text = models.TextField(verbose_name='Текст жалобы')
+    text = models.TextField(
+            verbose_name='Текст жалобы',
+        )
 
 
 class Owner(models.Model):
